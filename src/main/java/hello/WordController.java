fb707c85bca6fd222bc56397241e53d1f046b069
sameertiwari047 <sameertiwari047@gmail.com>
2018-10-10 19:36:53 +0530

    Update WordController.java

--- src/main/java/hello/WordController.java
+++ src/main/java/hello/WordController.java
@@ -1,8 +1,33 @@
 package hello;
 
+
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class WordController {
     // TODO Implement the /words/{word} endpoint
+	CheckPalindrome ch= new CheckPalindrome();
+	@RequestMapping(value="/words/{word}", method=RequestMethod.GET)
+	public  Palindrome checkWord(@PathVariable(value="word") String word){
+		
+		
+
+		Palindrome p= new Palindrome();
+		
+		     p.setWord(word);
+		     p.setPalindrome(ch.isPalindrom(word));
+		     p.setAnagramOfPalindrome(ch.canFormPalindrome(word));
+		     
+		     return p;
+		
+		
+	}
 }
+
